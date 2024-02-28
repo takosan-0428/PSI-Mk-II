4b84ffc707b048c3344561ec9907ada2fd508eac
yuuya-hoshino <sjlthmfjx@icloud.com>
2024-02-28 23:13:40 +0900

    単語帳一覧画面完成

--- src/frontend/src/posts/index.jsx
+++ src/frontend/src/posts/index.jsx
@@ -5,10 +5,24 @@ import { Link } from "react-router-dom";
 const Post = () => {
     return(
         <div>
-        <h2>個人</h2>
+        <h2>単語帳一覧</h2>
         <ul>
+          <li><Link to="/show">
+            <h3>ネットワーク</h3>
+            <div>更新日:00:00:00</div>
+            <div>作成日:00:00:00</div>
+        　</Link></li>
+          <li><Link to="/show">
+            <h3>データベース</h3>
+            <div>更新日:00:00:00</div>
+            <div>作成日:00:00:00</div>
+        　</Link></li>
+          <li><Link to="/show">
+            <h3>セキュリティ</h3>
+            <div>更新日:00:00:00</div>
+            <div>作成日:00:00:00</div>
+        　</Link></li>
           <li><Link to="/create">単語帳作成</Link></li>
-          <li><Link to="/show">単語帳詳細</Link></li>
         </ul>
       </div>
     );
